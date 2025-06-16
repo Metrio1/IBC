a59ea2a48886ab625ac4d0a87fcbc4a5e29ca5b2
Dmitriy Zakirov <metrio@inbox.ru>
2025-06-16 17:08:31 +0500

    refactor: html correction

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import.meta.glob("../styles/**/*.scss", { eager: true });
+
+const mobileOverlay = document.getElementById('mobileOverlay');
+
+mobileOverlay.addEventListener('click', (e) => {
+    const rect = mobileOverlay.getBoundingClientRect();
+    const isClickOutside =
+        e.clientX < rect.left || e.clientX > rect.right ||
+        e.clientY < rect.top || e.clientY > rect.bottom;
+
+    if (isClickOutside) {
+        mobileOverlay.close();
+    }
+});
+
+mobileOverlay.addEventListener('click', (event) => {
+    if (event.target === mobileOverlay) {
+        mobileOverlay.close();
+    }
+});
